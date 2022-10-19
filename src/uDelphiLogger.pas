e830ac4243b902ffb37d6103c1a389236ca76dc4
ALFHA - Eckert <FelixEckert@alfha.local>
2022-10-19 15:59:11 +0200

    Fix tab size

--- src/uDelphiLogger.pas
+++ src/uDelphiLogger.pas
@@ -32,83 +32,84 @@ unit uDelphiLogger;
 (* OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.           *)
 
 interface
-    uses SysUtils, DateUtils;
+  uses SysUtils, DateUtils;
 
-    type
-        TLogger = class
-            private
-                FLogFile: TextFile;
-                FSaveToFile: Boolean;
-            public
-                FLoggerFormatSettings: TFormatSettings;
-                FLoggerName: String;
-                FSilent: Boolean;
-                constructor Create; overload;
-                constructor Create(const ALogFile: String); overload;
-                destructor Free;
-                procedure LogF(const ALogLevel: Integer; const AMsg: String; const AArgs: array of const);
-                procedure Log(const ALogLevel: Integer; const AMsg: String);
-        end;
+  type
+    TLogger = class
+      private
+        FLogFile: TextFile;
+        FSaveToFile: Boolean;
+      public
+        FLoggerFormatSettings: TFormatSettings;
+        FLoggerName: String;
+        FSilent: Boolean;
+        constructor Create; overload;
+        constructor Create(const ALogFile: String); overload;
+        destructor Free;
+        procedure LogF(const ALogLevel: Integer; const AMsg: String; const AArgs: array of const);
+        procedure Log(const ALogLevel: Integer; const AMsg: String);
+      end;
 
-    const
-        {   LOG LEVELS   }
-        { Debug Message }
-        LOG_LVL_DBG = 0;
+  const
+    {   LOG LEVELS   }
+    { Debug Message }
+    LOG_LVL_DBG = 0;
 
-        { Information Message }
-        LOG_LVL_INF = 1;
+    { Information Message }
+    LOG_LVL_INF = 1;
 
-        { Warning Message } 
-        LOG_LVL_WARN = 2;
+    { Warning Message }
+    LOG_LVL_WARN = 2;
 
-        { Error Message }
-        LOG_LVL_ERR = 3;
+    { Error Message }
+    LOG_LVL_ERR = 3;
+
 implementation
-    constructor TLogger.Create;
-    begin
-        FLoggerFormatSettings := FormatSettings;
-        
-        FSaveToFile := False;
-        FSilent := False;
-        FLoggerName := '';
+  constructor TLogger.Create;
+  begin
+    FLoggerFormatSettings := FormatSettings;
+
+    FSaveToFile := False;
+    FSilent := False;
+    FLoggerName := '';
+  end;
+
+  constructor TLogger.Create(const ALogFile: String);
+  begin
+    Create;
+
+    FSaveToFile := True;
+    AssignFile(FLogFile, ALogFile);
+    Rewrite(FLogFile);
+  end;
+
+  destructor TLogger.Free;
+  begin
+    Close(FLogFile);
+  end;
+
+  procedure TLogger.LogF(const ALogLevel: Integer; const AMsg: String; const AArgs: array of const);
+  begin
+    Log(ALogLevel, Format(AMsg, AArgs));
+  end;
+
+  procedure TLogger.Log(const ALogLevel: Integer; const AMsg: String);
+  var
+    msg: String;
+  begin
+    msg := DateTimeToStr(Now(), FLoggerFormatSettings)+' ';
+
+    if (FLoggerName <> '') then msg := msg + FLoggerName + ' ';
+
+    case ALogLevel of
+      LOG_LVL_DBG: msg :=  msg + '[DEBUG] ';
+      LOG_LVL_INF: msg :=  msg + '[INFO] ';
+      LOG_LVL_WARN: msg := msg + '[WARN] ';
+      LOG_LVL_ERR: msg :=  msg + '[ERROR] ';
     end;
 
-    constructor TLogger.Create(const ALogFile: String);
-    begin
-        Create;
-
-        FSaveToFile := True;
-        AssignFile(FLogFile, ALogFile);
-        Rewrite(FLogFile);
-    end;
-
-    destructor TLogger.Free;
-    begin
-        Close(FLogFile);
-    end;
-
-    procedure TLogger.LogF(const ALogLevel: Integer; const AMsg: String; const AArgs: array of const);
-    begin
-        Log(ALogLevel, Format(AMsg, AArgs));
-    end;
-
-    procedure TLogger.Log(const ALogLevel: Integer; const AMsg: String);
-    var
-        msg: String;
-    begin
-        msg := DateTimeToStr(Now(), FLoggerFormatSettings)+' ';
-
-        if (FLoggerName <> '') then msg := msg + FLoggerName + ' ';
-
-        case ALogLevel of
-            LOG_LVL_DBG: msg :=  msg + '[DEBUG] ';
-            LOG_LVL_INF: msg :=  msg + '[INFO] ';
-            LOG_LVL_WARN: msg := msg + '[WARN] ';
-            LOG_LVL_ERR: msg :=  msg + '[ERROR] ';
-        end;
-
-        msg := msg+AMsg;
-        if not FSilent then writeln(msg);
-        if (FSaveToFile) then writeln(FLogFile, msg+AMsg);
-    end;
+    msg := msg+AMsg;
+    if not FSilent then writeln(msg);
+    if (FSaveToFile) then writeln(FLogFile, msg+AMsg);
+  end;
 end.
